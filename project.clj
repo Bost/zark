3915ce08807c683584090d25ec00c755466f9e2e
thebost <thebost@gmail.com>
2012-03-27 02:47:48 +0200

    Some preparation for unspecified arity

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject zark "1.0.0-SNAPSHOT"
             :description "FIXME: write description"
-            :dependencies [[org.clojure/clojure "1.3.0"]]
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [vimclojure/server "2.3.0"]]
 
             :main zark.core
             ;; This namespace will get loaded automatically when you launch a repl.
--- src/zark/core.clj
+++ src/zark/core.clj
@@ -6,29 +6,69 @@
 (load "../zark/core")
 )
 
+(def fns [* +])
 
-(def fns [map apply])
-
-(defn exec [f a b o]
-; TODO name of current function: http://www.mail-archive.com/clojure@googlegroups.com/msg13018.html
- (let [
- fi (fn [] (f a b))
- sf (str "(" 'f " " a " " b")")
- ]
-  (
-   (println (str sf ": " (fi)))
-   (if (= (fi) o)
-	(println (str "= " sf " " o))
-	(println (str "not= " sf " " o))
-   ))))
-
-(defn zark [a o]
-  (if (= a o)
-    (println "=" a o)
-
-    (map #(% a) fns)
-    ;(println "not=" a o)
-    
+(defn z [p1 result]
+  "Function for one parameter p1 and a result"
+  (doseq [fi fns]
+  (if (= (fi p1) result)
+    (println fi p1 "=" result)
+    (println fi p1 "not=" result)
     ))
+  )
+
+(defn zz [p1 p2 result]
+  "Function for two parameters p1 p2 and a result"
+  (doseq [fi fns]
+  (if (= (fi p1 p2) result)
+    (println fi p1 p2 "=" result)
+    (println fi p1 p2 "not=" result)
+    ))
+  )
+
+"Print evaluated expression and return its result"
+(defmacro dbg[x]
+  `(let [x# ~x]
+     (println '~x "=" x#) x#
+     )
+  )
+
+(defmacro m-zz[p1 p2 result]
+  `(let [
+         p1# ~p1
+         p2# ~p2
+         result# ~result
+         ]
+     (zz p1# p2# result#)
+     (println '~p1 '~p2"=" result#) result#
+     )
+  )
+
+(defn generic-z
+  ([] nil)
+  ([p1] nil)
+  ([p1 result]
+   (z p1 result)
+   )
+  ([p1 p2 result]
+   (zz p1 p2 result)
+   )
+  ([p1 p2 p3 & result]
+   (println "(defn zzz [p1 p2 p3 & result]: i snot implemented)")
+   )
+  )
+
+(defn generic [p & args]
+  "Just a test function for unspecified arity"
+  (let [
+        p1 p
+        p2 (first args)
+        p2next (next args)
+        p3 (first p2next)
+        p3next (next p2next)
+        ]
+    (println "p1:" p1 "; p2:" p2 "; p3:" p3 "; p3next:" p3next)
+    )
+  )
 
 (println "Loaded")
