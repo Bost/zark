06d6233b0ea1c28a58f628871ec2c6e63c971e96
Bost <thebost@gmail.com>
2015-03-25 01:10:29 +0100

    category_theory.clj: contracts and guarded functions

--- src/zark/category_theory.clj
+++ src/zark/category_theory.clj
@@ -1,7 +1,64 @@
 (ns zark.category-theory)
 
-(defn contract-str [s]
-  (if (= java.lang.String (type s))
+(defn cStr
+  "Contract for string objects in memory; a Category Theory object"
+  [s]
+  (if (instance? java.lang.String s)
     s
     (throw (Exception.
-            (str "Expecting (= java.lang.String (type " s "))")))))
+            (str "(instance? java.lang.String " s ") is false)")))))
+
+(defn cAny
+  "c - contract; a Category Theory object"
+  [x]
+  x)
+
+(defn cNum
+  "Contract for number objects in memory; a Category Theory object"
+  [n]
+  (if (instance? java.lang.Number n)
+    n
+    (throw (Exception.
+            (str "(instance? java.lang.Number " n ") is false")))))
+
+(defn cClass
+  ;; "Contract for class names; against objects in memory ; a Category Theory object"
+  [c]
+  (if (instance? java.lang.Class c)
+    c
+    (throw (Exception.
+            (str "(instance? java.lang.Class " c ") is false")))))
+
+(defn typeOf
+  "Create contract for objects in memory; a Category Theory object
+  pType must be an instance of a java.lang.Class"
+  [pType]
+  (let [cpType (cClass pType)]
+    (fn [p]
+      (if (instance? cpType p)
+        p
+        (throw (Exception.
+                (str "Expecting (instance? " cpType " " p ")")))))))
+
+(def cBool (typeOf Boolean))
+(def cObj (typeOf Object))
+(def cNum (typeOf Number))
+(def cStr (typeOf String))
+
+;; Compile error: Unable to resolve symbol: IPersistentCollection in this context
+;; (def gfColl (cTypeOf IPersistentCollection))
+(def cColl (cTypeOf clojure.lang.IPersistentCollection))
+
+;;;;;;;;;;;;;;;;;;;;
+
+(defn gfRepeat
+  "gf - guarded function; a Morphish"
+  [s]
+  (let [cs (cStr s)] ;; cs - checked s
+    (str cs cs)))
+
+;; contracts with functions they guard form a category
+
+;; (defn double [x]
+;; let [cx (cStr x)]
+;;     (cStr (str cx"-"cx))))
--- test/zark/category_theory_test.clj
+++ test/zark/category_theory_test.clj
@@ -2,8 +2,48 @@
   (:require [clojure.test :refer :all]
             [zark.category-theory :refer :all]))
 
-(deftest test-contract-str
-  (testing "The contract-str"
-    (is "" (contract-str ""))
-    (is (thrown? Exception (contract-str 1)))))
+(deftest test-contract-String
+  (testing "Contract for strings"
+    (is "" (cStr ""))
+    (is (thrown? Exception (cStr 1)))))
 
+(deftest test-contract-Number
+  (testing "Contract for numbers"
+    (is 1 (cNum 1))
+    (is (thrown? Exception (cNum "")))))
+
+(deftest test-contract-Class
+  (testing "Contract Classes"
+    (is String (cClass String))
+    (is Number (cClass Number))
+    (is (thrown? Exception (cClass 1)))
+    (is (thrown? Exception (cClass "")))
+    ))
+
+(deftest test-contract-creator-typeOf
+  (testing ""
+    (is Boolean (typeOf Boolean))
+    (is Number (typeOf Number))
+    (is String (typeOf String))
+    (is Object (typeOf Object))
+    (is clojure.lang.IPersistentCollection (typeOf clojure.lang.IPersistentCollection))
+    ;; Compile error: Unable to resolve symbol: IPersistentCollection in this context
+    ;; (is IPersistentCollection (typeOf IPersistentCollection))
+    (is (thrown? Exception (typeOf "Uhu")))
+    (is (thrown? Exception (typeOf 'Uhu)))
+    (is (thrown? Exception (typeOf `Uhu)))
+    ))
+
+(deftest test-cColl
+  (testing ""
+    (let [cColl (typeOf clojure.lang.IPersistentCollection)]
+      (is clojure.lang.IPersistentCollection (cColl []))
+      (is clojure.lang.IPersistentCollection (cColl [1 2 3]))
+      (is clojure.lang.IPersistentCollection (cColl '[1 2 3]))
+      (is clojure.lang.IPersistentCollection (cColl `[1 2 3]))
+      (is clojure.lang.IPersistentCollection (cColl {}))
+      (is clojure.lang.IPersistentCollection (cColl ()))
+      (is clojure.lang.IPersistentCollection (cColl #{}))
+      (is (thrown? Exception (cColl "[1 2 3]")))
+      (is (thrown? Exception (cColl 1)))
+      )))
