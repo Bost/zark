a990515b3bffdbf7fd77a60a1a894da17ebe576f
Bost <thebost@gmail.com>
2015-03-28 15:32:49 +0100

    Mighty ZarkOne!

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject zark "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Mighty ZarkOne!"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
