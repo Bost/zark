92d875c862d72c4f4c5bb3781e3c04932ad35dcd
Bost <thebost@gmail.com>
2015-08-05 01:40:54 +0200

    page 29

--- src/zark/reasoned-schemer.clj
+++ src/zark/reasoned-schemer.clj
@@ -273,6 +273,7 @@
         r))) ;; ((_0 _1))
 
 (def car first)
+(def cdr rest)
 
 ;; (clojure.repl/doc firsto)
 (car (list 'a 'c 'o 'r 'n)) ;; a
@@ -317,3 +318,135 @@
     (== 'pear x)))
 
 ;; page 26; the caro definition
+
+(cons
+ (car (list 'grape 'raisin 'pear))
+ (car (list (list 'a) (list 'b) (list 'c)))) ;; (grape (a) (b) (c)))
+
+(run* [r]
+  (fresh [x y]
+    (caro #_firsto (list 'grape 'raisin 'pear) x) ;; x: grape
+    (caro #_firsto (list (list 'a) (list 'b) (list 'c)) y) ;; y: (list a)
+    (== (lcons x y) r))) ;; ((grape a))
+
+;; (clojure.repl/doc rest)
+(cdr (list 'grape 'raisin 'pear))
+
+(def cdro
+  "A function of 2 arguments - almost own deduction! Yay! :)"
+  (fn [p a]
+    (fresh [d]
+      (== (lcons d a) p))))
+
+(run* [r]
+  (fresh [v]
+    (cdro (list 'a 'c 'o 'r 'n) v)
+    (caro v r)))
+
+(run* [a]
+  ;; (clojure.repl/doc cdro)
+  (cdro (list 'grape 'raisin 'pear) a))
+
+(cons
+ (cdr (list 'grape 'raisin 'pear))
+ (car (list (list 'a) (list 'b) (list 'c))))
+;; (cons (list 'raisin 'pear) (list 'a)) => ((raisin pear) a)
+
+(run* [r]
+  (fresh [x y]
+    (cdro (list 'a 'c 'o 'r 'n) x)                ; x: (list 'c 'o 'r 'n)
+    (caro (list (list 'a) (list 'b) (list 'c)) y) ; y: (list 'a)
+    (== (lcons x y) r))) ; (lcons (list 'c 'o 'r 'n) (list 'a)) => ((c o r n) a) => (((c o r n) a))
+
+(run* [q]
+  (cdro (list 'a 'c 'o 'r 'n) (list 'c 'o 'r 'n))
+  (== t# q))
+
+(run* [l]
+  (fresh [x]
+    ;; it becomes clear if I change the order of expressions:
+    (cdro l (list 'c 'o 'r 'n))
+    (caro l x)
+    (== 'a x)))
+
+(run* [l]
+  (fresh [x]
+    ;; it becomes clear if I change the order of expressions:
+    (== 'a x)
+    (caro l x)                   ; (caro _0 'a)
+    (cdro l (list 'c 'o 'r 'n))  ; (cdro _0 (list 'c 'o 'r 'n))
+    ))
+;;caro and cdro are complementary - so probably I take take the lcons because ???
+;; (it belongs to the pack)
+
+(run* [l]
+  (conso (list 'a 'b 'c) (list 'd 'e) l)) ;; (((a b c) d e))
+
+(run* [x]
+  (conso x (list 'a 'b 'c) (list 'd 'a 'b 'c))) ;; (d)
+
+(run* [r]
+  (fresh [x y z]
+    (== (list 'e 'a 'd x) r)    ; r: (list 'e 'a 'd _0)
+    (conso y (list 'a z 'c) r))) ; (conso _1 (list 'a _2 'c) (list 'e 'a 'd _0))
+;; x: 'c
+;; r: ((list 'e 'a 'd 'c))
+;; z: 'd
+;; y: 'e
+
+(run* [x]
+  (conso x (list 'a x 'c) (list 'd 'a x 'c)));; d
+
+;; page 29
+
+(run* [l]
+  (fresh [x]
+    (== (list 'd 'a x 'c) l)      ; l: (list 'd 'a x 'c)
+    (conso x (list 'a x 'c) l)))
+;; (conso x (list 'a x 'c) (list 'd 'a x 'c)) => ((list 'd 'a 'd 'c))
+
+(def -cons0
+  (fn [a b c]
+    (== (lcons a b) c)))
+
+(def +cons0
+  (fn [a b c]
+    (caro c a)
+    (cdro c b)
+    (== (lcons a b) c)))
+
+(run* [x]
+  (-cons0 1 (list 2) x))
+
+(run* [x]
+  (+cons0 1 (list 2) x))
+
+(run* [x]
+  (-cons0 'a (list 'b) (list 'a 'b))
+  (== t# x))
+
+(run* [x]
+  (+cons0 'a (list 'b) (list 'a 'b))
+  (== t# x))
+
+(run* [x]
+  (fresh [u v]
+    (-cons0 'a (list 'b) u)
+    (+cons0 'a (list 'b) v)
+    (== u v)
+    (== t# x)))
+
+(run* [x]
+  (fresh [a b u v]
+    (-cons0 a (list b) u)
+    (+cons0 a (list b) v)
+    (== u v)
+    (== t# x)))
+
+(run* [x]
+  (fresh [a b]
+    (-cons0 a (list b) (list a b))
+    (+cons0 a (list b) (list a b))
+    (== t# x)))
+
+;; page 29
