3dee3ab38dd7070e6fb6a313d7302dd492732f26
Bost <thebost@gmail.com>
2017-08-25 11:55:10 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
-  [[org.clojure/clojure "1.9.0-alpha17"]
-   [com.datomic/datomic-free "0.9.5561.54" :exclusions [joda-time]]
+  [[org.clojure/clojure "1.9.0-alpha19"]
+   [com.datomic/datomic-free "0.9.5561.56" :exclusions [joda-time]]
    [org.clojure/core.logic "0.8.11"]
    [org.clojure/algo.monads "0.1.6"]
 
@@ -12,7 +12,7 @@
    [sablono "0.8.0"] ; hiccup style templating for om-next
 
    ;; webapp - begin
-   [org.clojure/clojurescript "1.9.854" :exclusions [com.google.guava/guava]]
+   [org.clojure/clojurescript "1.9.908" :exclusions [com.google.guava/guava]]
    [prismatic/om-tools "0.4.0"] ; more convenient dom elements
    [org.omcljs/om "1.0.0-alpha22" :exclusions [commons-codec]]
    ;; describe how a web server communicates with web apps
@@ -61,7 +61,7 @@
    ]
   :plugins
   [[lein-cljsbuild "1.1.7"]
-   [lein-figwheel "0.5.12" :exclusions [org.clojure/clojure]]
+   [lein-figwheel "0.5.13" :exclusions [org.clojure/clojure]]
    #_[refactor-nrepl "2.2.0"]
    #_[cider/cider-nrepl "0.14.0"]]
   ;; TODO: uncomment for autorun :main zark.core
@@ -80,9 +80,10 @@
    :http-server-root "public" ; css-dirs requires http-server-root specification
    :css-dirs ["resources/public/css"]
    ;; Load CIDER, refactor-nrepl and piggieback middleware
-   :nrepl-middleware ["cider.nrepl/cider-middleware"
-                      "refactor-nrepl.middleware/wrap-refactor"
-                      "cemerick.piggieback/wrap-cljs-repl"]}
+   ;; :nrepl-middleware ["cider.nrepl/cider-middleware"V
+   ;;                    ;; "refactor-nrepl.middleware/wrap-refactor"
+   ;;                    "cemerick.piggieback/wrap-cljs-repl"]
+   }
 
   :cljsbuild
   {:builds [{:id "dev"
@@ -96,14 +97,15 @@
                         :optimizations :none}}]}
   :profiles
   {:uberjar {:aot :all}
-   :dev {:dependencies [[figwheel-sidecar "0.5.12"
+   :dev {:dependencies [[figwheel-sidecar "0.5.13"
                          :exclusions [com.google.guava/guava
                                       commons-codec
                                       org.clojure/tools.analyzer
                                       org.clojure/tools.analyzer.jvm]]
                         [org.clojure/test.check "0.9.0"] ; for clojure.spec
                         [com.cemerick/piggieback "0.2.2"]
-                        [org.clojure/tools.nrepl "0.2.13"]]
+                        ;; [org.clojure/tools.nrepl "0.2.13"]
+                        ]
          ;; Leads to Error loading cemerick.piggieback ... /queue-eval is not public
          ;; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
          :source-paths ["src/cljs" "src/clj"]}})
