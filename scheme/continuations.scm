ebaab3d18064903b1c4feee5b1e4235830e2c027
Bost <thebost@gmail.com>
2019-02-11 19:54:32 +0100

    backup

--- scheme/continuations.scm
+++ scheme/continuations.scm
@@ -1,78 +1,120 @@
-;; M-x run-geiser
+;; -*- origami-fold-style: triple-braces -*-
+;; start scheme repl: M-x run-geiser
+;; folding: M-x origami-forward-toggle-node / z a
 
-(define (f) 1) ;; constant function f: Any -> 1
+;; constant function fc: AnyType (ignored & ignored) -> Number
+(define (fc) 1)
 
-(define (foo)
-  (display "hello\n")
-  ;; (display (bar)) (newline)
-  (exit))
+;; function of one argument fhw: String (infered) -> String
+(define (fhw arg) (string-append "Hello World, " arg))
 
-;; https://www.gnu.org/software/guile/manual/html_node/Continuations.html
+;; Motivational example {{{
+;; [1] loop-fn is a compound-procedure of one argument `ls'
+;; [2] If `ls' is '() then return the identity of `*', finish the current loop
+;;     and proceed to the next elem of the `ls'
+;; [3] If the first elem of `ls' is `0' then return the absorbing elem of `*',
+;;     finish the current loop and proceed to the next elem of the `ls'.
+;;     Inefficient :-(
+;; [4] Multiply & loop around
+;; See also http://www.cs.sfu.ca/CourseCentral/383/havens/notes/Lecture06.pdf"
+(define (product init-ls)
+  "Multiply the elements of a list. Inefficient"
+  (let loop-fn ((ls init-ls))                       ;; [1]
+    (cond
+     ((null? ls) 1)                                 ;; [2]
+     ((= (car ls) 0) 0)                             ;; [3]
+     (else (* (car ls) (loop-fn (cdr ls)))))))      ;; [4]
 
+(product '(1 2 3 4 5))     ;; => 120
+(product '(7 3 8 0 1 9 5)) ;; => 0
+(product '())              ;; => 1
+;; }}}
+
+;; Show inefficiency {{{
+(define (to-str arg)
+  "Helper function. Convert `arg' to string."
+  (cond
+   ((string? arg) arg)
+   ((number? arg) (number->string arg))
+   (else "to-str not implemented for this type")))
+
+(define (product init-ls)
+  "Inefficient & buggy"
+  (string-append
+   "The result is: "
+   (to-str
+    (let loop-fn ((ls init-ls))
+      (cond
+       ((null? ls) 1)
+       ((= (car ls) 0) " a bloody 0. Haha!")
+       (else (* (car ls) (loop-fn (cdr ls)))))))))
+(product '(1 2 3 4 5))     ;; => "The result is: 120"
+(product '())              ;; => "The result is: 1"
+(product '(7 3 8 0 1 9 5)) ;; => Wrong type
+;; }}}
+
+;; A better way {{{
+;; [1] Create continuation function `break' wrapping around the current
+;;     computation - the `(let loop-fn ...)'
+;; [2] loop-fn is a compound-procedure of one arg `ls'
+;; [3] If `ls' is '() then return the identity of `*', finish the current loop
+;;     and proceed to the next elem of the `ls'
+;; [4] Continue with the rest of the computation
+;;         `(string-append "The result is: " (to-string ...))'
+;;     with the value of current computation being 0 thus effectivelly breaking
+;;     out of the loop.
+;; [5] Multiply & loop around
+;; See also http://www.cs.sfu.ca/CourseCentral/383/havens/notes/Lecture06.pdf"
+(define (product init-ls)
+  "Multiply the elements of a list. Efficient but malevolent."
+  (string-append
+   "The result is: "
+   (to-str
+    (call/cc
+     (lambda (break)                                         ;; [1]
+       (let loop-fn ((ls init-ls))                      ;; [2]
+         (cond
+          ((null? ls) 1)                                ;; [3]
+          ((= (car ls) 0) (break " a bloody 0. Haha!")) ;; [4]
+          (else (* (car ls) (loop-fn (cdr ls))))))))))) ;; [5]
+
+(product '(1 2 3 4 5))     ;; => "The result is: 120"
+(product '())              ;; => "The result is: 1"
+(product '(7 3 8 0 1 9 5)) ;; => "The result is: 0"
+;; }}}
+
+;; {{{
 ;; `call/cc' alias for `call-with-current-continuation'
+;; https://www.gnu.org/software/guile/manual/html_node/Continuations.html
 (call-with-current-continuation (lambda (k) 1))
 (call/cc (lambda (k) 1))
+;; }}}
 
 (((call/cc
-   (lambda (k)
-     k))
+   (lambda (k) k))
   (lambda (x) x))
  "HEY!")
 
-;; http://www.cs.sfu.ca/CourseCentral/383/havens/notes/Lecture06.pdf
-;; product is a fn of one argument
-;; [1] create continuation fn `break' wrapping around the current computation,
-;;     the `(let ...)'
-;; [2] f is a compound-procedure of one arg `ls'
-;; [3] if `ls' is '() then return the identity of * and terminate
-;; [4] continue with the rest of the computation
-;;         `(string-append "The result is: " (number->string ...))'
-;;     with the value of current computation being 0 thus effectivelly breaking
-;;     out of the loop. Note: `(break "0 haha!")' gives incorrect type error.
-;; [5] multiply & loop around
-(define product
-  (lambda (initial-ls)
-    (string-append
-     "The result is: "
-     (number->string
-      (call/cc
-       (lambda (break)                                          ;; [1]
-         (let loop-fn ((ls initial-ls))                    ;; [2]
-           (cond
-            ((null? ls) 1)                                 ;; [3]
-            ((= (car ls) 0) (break 0))                     ;; [4]
-            (else (* (car ls) (loop-fn (cdr ls)))))))))))) ;; [5]
-
-(product '(1 2 3 4 5))     ;; => "The result is: 120"
-(product '(7 3 8 0 1 9 5)) ;; => "The result is: 0"
-(product '())              ;; => "The result is: 1"
-
+;; {{{
 (call/cc
  (lambda (k)
    ;; (error (k "foo"))
    ;; the current computation `(/ 30 5 3)' is effectivelly ignored
    (/ 30 5 (k 1) 3)))
 ;; => 1
+;; }}}
 
-(define (foo return)
-  (return 2)
-  3)
-
-(define stuff (foo return)
-  (return 2)
-  3)
-
-(foo (lambda (x) x))
-;; => 3
-
-(foo (lambda (x) 5))
-;; => 3
-
-
+;; {{{
+(define (foo return) (return 2) 3)
+(define (baz arg)    (arg 5) 7)
 (call/cc foo)   ;; => 2
 (call/cc (foo)) ;; => error
+(foo (lambda (x) x)) ;; => 3
+(foo (lambda (x) 5)) ;; => 3
+;; }}}
 
-(define *x* '())
+(define *x* '())  ;; global definition
+
 (lambda (x)
   (set! *x* x)
   (+ 1 (x 2) 3))
