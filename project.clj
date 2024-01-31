6ed0620e0ac264784afbca76b98a8571758cfec2
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2024-01-31 17:12:50 +0100

    lein ancient upgrade :all :interactive :check-clojure :no-tests

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [
-   [org.clojure/clojure "1.10.3"]
-   [org.clojure/spec.alpha "0.2.194"]
-   [defun "0.3.1"] ;; macro to define clojure functions with pattern matching just as erlang or elixir
+   [org.clojure/clojure "1.11.1"]
+   [org.clojure/spec.alpha "0.3.218"]
+   [defun "0.4.0"] ;; macro to define clojure functions with pattern matching just as erlang or elixir
    [swiss-arrows "1.0.0"] ;; arrow macros
    [com.datomic/datomic-free "0.9.5697" :exclusions [joda-time]]
    ;; see https://dev.clojure.org/jira/browse/CRRBV-18
    [org.clojure/core.rrb-vector "0.1.2"]
-   [org.clojure/core.logic "1.0.0"]
+   [org.clojure/core.logic "1.0.1"]
    [org.clojure/algo.monads "0.1.6"]
    [org.clojure/algo.generic "0.1.3"]
 
@@ -19,17 +19,17 @@
    [sablono "0.8.6"] ; hiccup style templating for om-next
 
    ;; webapp - begin
-   [re-frame "1.2.0"]
+   [re-frame "1.4.3"]
    [secretary "1.2.3"]
-   [org.clojure/clojurescript "1.10.773" :exclusions [com.google.guava/guava]]
+   [org.clojure/clojurescript "1.11.132" :exclusions [com.google.guava/guava]]
    [prismatic/om-tools "0.5.0"] ; more convenient dom elements
    [org.omcljs/om "1.0.0-alpha22" :exclusions [commons-codec]] ;; is this needed?
 
    ;; Ring routing lib; dispatching of GET, PUT, etc.
    ;; describe how a web server communicates with web apps
    ;; and how web apps can be chained together to process one request
-   [ring "1.9.2"]
-   [compojure "1.6.2"]
+   [ring "1.11.0"]
+   [compojure "1.7.0"]
    ;; [compojure "1.6.1" :exclusions [commons-codec org.clojure/tools.macro]]
    ;; [garden "1.3.9"] ; render CSS; see the :garden config below pf
    [com.andrewmcveigh/cljs-time "0.5.2"] ;; (time/now) in cljs
@@ -40,25 +40,25 @@
    [org.clojure/java.jdbc "0.7.12"]
    [com.mchange/c3p0 "0.9.5.5"] ; db connection pooling
    ;; TODO https://github.com/LauJensen/clojureql
-   [mysql/mysql-connector-java "8.0.23"]
+   [mysql/mysql-connector-java "8.0.33"]
    ;; http://ianrumford.github.io/blog/2012/11/17/first-take-on-contracts-in-clojure/
    [org.clojure/core.contracts "0.0.6"]
-   [org.clojure/core.memoize "1.0.236"]
+   [org.clojure/core.memoize "1.0.257"]
 
-   [org.clojure/core.match "1.0.0"] ; pattern matching library
+   [org.clojure/core.match "1.0.1"] ; pattern matching library
 
-   [org.clojars.bost/utils "0.0.0-35-0x665f"] ;; pulls in also clj-time-ext
+   [org.clojars.bost/utils "0.0.0-49-0x726c"] ;; pulls in also clj-time-ext
 
    ;; quartzite dependency on slf4j-api should be auto-resolved
    ;; [org.slf4j/slf4j-nop "1.7.13"] ; Simple Logging Facade for Java
-   [clojurewerkz/quartzite "2.1.0"] ; scheduling
+   [clojurewerkz/quartzite "2.2.0"] ; scheduling
    ;; [korma "0.4.0"] ;; sql for clojure
    ;; [onelog "0.4.5"]  ;; used also by ring
 
    ;; [com.draines/postal "1.11.3"]            ; sending emails
    ;; [org.apache.commons/commons-email "1.4"] ; sending emails
    [clj-ssh "0.5.14"]
-   [funcool/cuerdas "2020.03.26-3"] ; string manipulation - (str/surround % "'")
+   [funcool/cuerdas "2023.11.09-407"] ; string manipulation - (str/surround % "'")
    [org.clojure.typed/runtime.jvm "1.0.1"]
 
    ;; [cauchy-jobs-kestrel "0.1.0"] ;; distributed message queue on the JVM
@@ -72,27 +72,27 @@
    ;; ;;Formalization of integers in LaTTe.
    ;; [latte-integers "0.9.0-SNAPSHOT"]
 
-   [funcool/cats "2.4.1"]
+   [funcool/cats "2.4.2"]
 
    ;; logging doesn't work out-of-the-box
    ;; [org.clojure/tools.logging "0.4.1"]
    ;; debug single- and multi-threaded apps
    ;; [spyscope "0.1.6"]
 
-   [org.python/jython-standalone "2.7.2"]
+   [org.python/jython-standalone "2.7.3"]
    ;; Clojure Jython interop
    [clojure-python "1.0.0"]
-   [midje "1.9.10"]
+   [midje "1.10.10"]
 
 
-   [halfling "1.2.1"]
+   [halfling "1.3.1"]
 
-   [org.clojure/data.json "2.0.2"]
+   [org.clojure/data.json "2.5.0"]
    ;; Core.async is not provided as part of Clojure's standard distribution, and
    ;; must be included as a dependency.
-   [org.clojure/core.async "1.3.610"]
+   [org.clojure/core.async "1.6.681"]
 
-   [net.cgrand/xforms "0.19.2"]
+   [net.cgrand/xforms "0.19.6"]
    ]
   :plugins
   [;; a plugin to tell you your code is bad, and that you should feel bad
@@ -143,11 +143,11 @@
                                       commons-codec
                                       org.clojure/tools.analyzer
                                       org.clojure/tools.analyzer.jvm]]
-                        [org.clojure/test.check "1.1.0"] ; for clojure.spec
-                        [cider/piggieback "0.5.2"]
+                        [org.clojure/test.check "1.1.1"] ; for clojure.spec
+                        [cider/piggieback "0.5.3"]
                         ;; [com.cemerick/piggieback "0.2.2"]
                         [ns-tracker "0.4.0"]
-                        [binaryage/devtools "1.0.2"]
+                        [binaryage/devtools "1.0.7"]
                         [org.clojure.typed/checker.jvm "1.0.1"]
                         ]
          ;; Leads to Error loading cemerick.piggieback ...
